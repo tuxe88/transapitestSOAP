fd45d8a48e2d91e67d1fd4d066e064fd024775f2
paulofer85 <paulofer85@gmail.com>
2018-10-23 21:58:05 -0300

    Revert "Merge branch 'stg' into dev"
    This reverts commit af85b8e1a797abcaba9a27b21986e7abf0e0237b.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 source venv/bin/activate
 export FLASK_APP=soap
-export FLASK_ENV=staging
-export HOST_GDSAPI=http://gdsapi.munben.com/
+export FLASK_ENV=development
+export HOST_GDSAPI=http://mozart.microblet.com/
 nohup flask run --host=0.0.0.0 --port=5010 &
 deactivate
