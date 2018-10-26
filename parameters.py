f7012fa5bc7d6b081cdb63be79a467390e39a5db
Valentín Augusto Tourrilhes <vtourrilhes@hcdn.gob.ar>
2018-10-26 13:59:36 -0300

    agregado el archivo nohup a gitignore

--- parameters.py
+++
@@ -1,6 +0,0 @@
-import os
-
-HOST_GDS_API = os.getenv('HOST_GDSAPI')
-API_PORT = os.getenv('API_PORT')
-GLOBAL_NAMESPACE = os.getenv('HOST_GDSAPI')+'cnet/flight/'
-STRUCTURE_NAMESPACE = os.getenv('HOST_GDSAPI')+'cnet/structure/'
